3bc6b0a053acba7b8b3a77a500f012c52e9ce3bc
SanZ877 <ahsan.arrizqi2007@gmail.com>
2026-06-15 15:53:34 +0700

    hapus docker compose dan update server_data.php

--- server_data.php
+++ server_data.php
@@ -3,6 +3,6 @@ port|17091
 type|1
 type2|1
 #maint|Server under maintenance. Please try again later.
-loginurl|sanzgt.duckdns.org
+loginurl|sanzgt.duckdns.org/player
 meta|gurotopia
 RTENDMARKERBS1001
