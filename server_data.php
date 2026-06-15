75261b87125420fb00c641eef53345fbb05a77bc
SanZ877 <ahsan.arrizqi2007@gmail.com>
2026-06-15 15:03:16 +0700

    pindah docker-compose ke luar repo

--- server_data.php
+++ server_data.php
@@ -3,6 +3,6 @@ port|17091
 type|1
 type2|1
 #maint|Server under maintenance. Please try again later.
-loginurl|login-gurotopia-five.vercel.app
+loginurl|sanzgt.duckdns.org
 meta|gurotopia
 RTENDMARKERBS1001
